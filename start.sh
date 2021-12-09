0ed0ff81d7ae4dc62e2a9a48ba37c29ea05f3a4f
H3xept <h3xept@gmail.com>
2021-12-09 17:37:28 +0100

    Added startup script

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+VENV_LOCATION=$1;
+
+if ! [[ -d $VENV_LOCATION ]]; then
+    echo "Virtual environment not present. Creating virtual environment named 'venv' at $VENV_LOCATION.";
+    python3 -m venv $VENV_LOCATION;
+fi
+
+source ./venv/bin/activate
+
+docker-compose build
+docker-composer up
